1081d9bbb1760c95b3fffa33c942bf694dce29a9
majunjie <androidmumo@outlook.com>
2022-09-10 17:21:34 +0800

    feat: 增加夜间模式切换动画

--- src/components/h5-header.vue
+++ src/components/h5-header.vue
@@ -1,6 +1,53 @@
 <script setup lang="ts">
 const { isDark, toggleDark } = useDarks()
 const { t, toggleLocale, language } = useLanguage()
+
+const state = reactive({
+	showSleep: false,
+	showWake: false,
+	animation: false,
+	disableClickDark: false,
+})
+
+// 初始化显示状态
+state.showSleep = !isDark.value
+state.showWake = !!isDark.value
+
+// 点击sleep
+const clickSleep = () => {
+	if (state.disableClickDark) return
+	state.disableClickDark = true
+	state.animation = true
+	toggleDark(true)
+	setTimeout(() => {
+		state.showWake = true
+	}, 100)
+	setTimeout(() => {
+		state.showSleep = false
+	}, 500)
+	setTimeout(() => {
+		state.animation = false
+		state.disableClickDark = false
+	}, 600)
+}
+
+// 点击wake
+const clickWake = () => {
+	if (state.disableClickDark) return
+	state.disableClickDark = true
+	state.animation = true
+	toggleDark(false)
+	setTimeout(() => {
+		state.showSleep = true
+	}, 100)
+	setTimeout(() => {
+		state.showWake = false
+	}, 500)
+	setTimeout(() => {
+		state.animation = false
+		state.disableClickDark = false
+	}, 600)
+}
 </script>
 
 <template>
@@ -15,13 +62,61 @@ const { t, toggleLocale, language } = useLanguage()
 				<span class="text">{{ language }}</span>
 				<i-ion:language-outline class="icon" @click="toggleLocale()" />
 			</div>
-			<i-carbon:asleep v-if="!isDark" @click="toggleDark()" />
-			<i-carbon:awake v-if="isDark" @click="toggleDark()" />
+			<div class="dark-btn">
+				<i-carbon:asleep
+					v-if="state.showSleep"
+					:class="{
+						icon: true,
+						'dark-leave': state.animation && isDark,
+						'dark-enter': state.animation && !isDark,
+					}"
+					@click="clickSleep"
+				/>
+				<i-carbon:awake
+					v-if="state.showWake"
+					:class="{
+						icon: true,
+						'dark-leave': state.animation && !isDark,
+						'dark-enter': state.animation && isDark,
+					}"
+					@click="clickWake"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
 
 <style scoped lang="scss">
+@keyframes darkLeave {
+	0% {
+		transform: scale(1);
+		opacity: 1;
+	}
+	100% {
+		transform: scale(3);
+		opacity: 0;
+	}
+}
+
+@keyframes darkEnter {
+	0% {
+		transform: scale(0.1);
+	}
+	100% {
+		transform: scale(1);
+	}
+}
+
+.dark-leave {
+	animation-name: darkLeave;
+	animation-duration: 0.5s;
+}
+
+.dark-enter {
+	animation-name: darkEnter;
+	animation-duration: 0.5s;
+}
+
 .h5-header {
 	position: fixed;
 	z-index: 1;
@@ -61,8 +156,20 @@ const { t, toggleLocale, language } = useLanguage()
 				margin-right: 6px;
 			}
 		}
+		.dark-btn {
+			width: 14px;
+			height: 14px;
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.icon {
+				position: absolute;
+			}
+		}
 	}
 }
+
 html.dark .h5-header {
 	background-color: rgba(0, 0, 0, 0.72);
 }
