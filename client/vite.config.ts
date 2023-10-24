35665b877974646fe08f3330f3d553dfabe3bcf5
white kong <androidmumo@outlook.com>
2023-10-24 20:09:36 +0800

    feat: 添加开发代理地址

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,6 +17,10 @@ export default defineConfig({
 	server: {
 		//使用IP能访问
 		host: '0.0.0.0',
+		proxy: {
+			'/api': 'http://bingtest.mcloc.cn',
+			'/img': 'http://bingtest.mcloc.cn',
+		},
 	},
 	resolve: {
 		alias: {
