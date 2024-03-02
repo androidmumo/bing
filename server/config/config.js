2e392542687adb72efea8f40f4fa760e36c4bbc6
jychen6688 <116131272+jychen6688@users.noreply.github.com>
2024-03-02 16:19:02 +0800

    123
    123

--- server/config/config.js
+++ server/config/config.js
@@ -1,30 +1,49 @@
-// 内部配置文件 config.js
+// 用户配置文件 config.js
 
-// 安装配置
-const installConfig = {
-  databaseVersion: 1,
-  dir: "data/resources", // 图片在服务端的真实保存路径 (相对于根目录、首尾不能为’/‘)
-  databaseTable: "bing", // 数据库表名-数据 (可在安装前更改)
-  databaseTableInfo: "info", // 数据库表名-信息 (可在安装前更改)
-}
-
-// API基础配置
-const apiBaseConfig = {
-  static: "img", // 图片静态托管路径 (url访问图片时的路径、首尾不能为’/‘)
-}
-
-// API接口后缀配置(接口url后缀、首尾不能为’/‘)
-const apiConfig = {
-  UPDATE: "api/update", // 手动更新图片（需要key）
-  DELETE: "api/delete", // 手动清理图片（需要key）
-  GET_IMAGE: "api/getImage", // 获取当天图片
-  GET_LIST: "api/getList", // 获取图片列表
-  GET_INFO: "api/getInfo", // 获取图片详情
-  GET_WEBINFO: "api/getWebInfo", // 获取网站信息
+// 基础配置
+const baseConfig = {
+  port: 3000, // 服务启动端口号
+  updateTime: "00:01:00", // 每天更新时间 (开始从必应官方服务器下载图片的时间)
+  DelayTime: 5, // 延迟时间（分钟） 即每天00:05：00的时候才显示当天的图片。性能较差的实例应适当调大此值 (仅针对'/api/getImage'接口)
+  surviveDays: 0, // 图片存活天数（即图片保存多少天，到期即清理） 0为不清理
+  retryTimeout: 10000, // 错误重试间隔。共重试10次，每次间隔时间递增，这里指的是首次间隔时间 (单位:ms)
+  key: 'abcdefgh', // 鉴权密钥。用于需要鉴权才能访问的接口
 };
 
+// 数据库配置 (注意：除数据库连接池大小外，以下配置项提及的内容需在安装前准备好并填入)
+const databaseConfig = {
+  host: "127.0.0.1", // 数据库链接地址
+  port: "3306", // 数据库连接端口
+  database: "bing", // 数据库名
+  user: "bing", // 数据库用户名
+  password: "bing", // 数据库密码
+  connectionLimit: 100, // 数据库连接池大小
+};
+
+// 网站信息配置
+const infoConfig = {
+  link: [
+    {
+      label: "花椒麻鱼的主页", // 链接名称
+      url: "https://www.hjfish.cn/" // 链接地址
+    },
+    {
+      label: "花椒麻鱼的主页",
+      url: "https://blog.hjfish.cn/"
+    }
+  ],
+  htmlSlot: {
+    beforeFooter: ``, // 页脚上方HTML插槽
+    afterFooter: `<a style="margin-right: 10px;" target="_blank" href="https://beian.miit.gov.cn/">晋ICP备20001086号-1</a>
+  <a style="margin-right: 10px; display: flex; align-items: center;" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41080202000141">
+    <img style="width: 14px; margin-right: 6px;" src="https://www.mcloc.cn/wp-content/uploads/2020/04/beiantubiao-19.png"/>
+    <span>豫公网安备 41080202000141号</span>
+  </a>` // 页脚下方HTML插槽
+  }
+}
+
 module.exports = {
-  installConfig,
-  apiBaseConfig,
-  apiConfig,
+  baseConfig,
+  databaseConfig,
+  infoConfig,
 };
